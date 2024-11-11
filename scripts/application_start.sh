80bb1b5ce4173eecbcc4c82c3b97e3b5b132185f
karthick <vikramkarthick2001@gmail.com>
2024-11-11 12:03:53 +0530

    nvidia

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,2 +1,2 @@
 echo "Running container..."
-sudo docker run -d -p 8501:8501 public.ecr.aws/u4i0c1o5/nvidiallm:latest
+ sudo docker run -d -p 8501:8501  -e NVIDIA_API_KEY="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" public.ecr.aws/u4i0c1o5/nvidiallm:latest
